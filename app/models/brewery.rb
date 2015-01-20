5f655c4a67ec0b0422429e69677c87581009016b
samutamm <samutamm@gmail.com>
2015-01-20 17:26:07 +0200

    Average_rating to brewery

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,6 @@
 class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
 
   def print_report
     puts name
@@ -10,5 +11,9 @@ class Brewery < ActiveRecord::Base
   def restart
     self.year = 2015
     puts "changed year to #{year}"
+  end
+
+  def average_rating
+    self.ratings.inject(0.0) { |sum, n| sum += n.score  } / self.ratings.count
   end
 end
