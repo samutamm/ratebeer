565afb016f2a61c917d90ed61069331a482966c6
samutamm <samutamm@gmail.com>
2015-01-31 11:37:08 +0200

    has-many-through connection between users and beers.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   include RatingAverage
   has_many :ratings
+  has_many :beers, through:  :ratings
 
   validates :username, uniqueness: true
   validates :username, length: { minimum: 3,
