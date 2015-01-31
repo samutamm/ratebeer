35b09a559c10379bde168e2c23583d74231b79c6
samutamm <samutamm@gmail.com>
2015-01-31 15:55:54 +0200

    Delete-feature to users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,13 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    @user.ratings.each {|x| x.destroy}
+    @user.memberships.each {|x| x.destroy}
+
     @user.destroy
+
+    session[:user_id] = nil
+
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
