3d596c3c96df1898b1ee0d7cc514f51cf8e7dd60
samutamm <samutamm@gmail.com>
2015-01-31 11:26:08 +0200

    Custom validation for brewery; checks that year is not in the future.

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -6,8 +6,14 @@ class Brewery < ActiveRecord::Base
 
   validates :name, presence: true
   validates :year, numericality: { greater_than_or_equal_to: 1042,
-                                    less_than_or_equal_to: 2015,
                                     only_integer: true }
+  validate :year_cannot_be_in_the_future
+
+  def year_cannot_be_in_the_future
+    if :year.present? && year > Time.now.year
+      errors.add(:year, "can't be in the future.")
+    end
+  end
 
   def print_report
     puts name
