499d5c1a5906d67e55af8435d360731f05ce6921
samutamm <samutamm@gmail.com>
2015-02-04 22:17:10 +0200

    More tests for beer-club feature.

--- spec/features/beer_club_spec.rb
+++ spec/features/beer_club_spec.rb
@@ -12,4 +12,35 @@ describe "Beer club page" do
     expect(page).to_not have_content 'Show'
     expect(page).to_not have_content 'Delete'
   end
+
+  it 'should list just created clubs.' do
+    expect{
+      FactoryGirl.create(:beer_club, name:"klubben")
+      FactoryGirl.create(:beer_club, name:"buu")
+    }.to change{BeerClub.count}.from(0).to(2)
+
+    visit beer_clubs_path
+    expect(page).to have_content 'buu'
+    expect(page).to have_content 'klubben'
+  end
+
+  it 'clicking show method should render clubs page.' do
+    FactoryGirl.create(:beer_club, name:"klubben")
+    visit beer_clubs_path
+
+    click_link('Show')
+    expect(page).to have_content 'The club has not yet any members'
+  end
+
+  it 'clicking show method should render clubs page.' do
+    FactoryGirl.create(:beer_club, name:"klubben")
+    visit beer_clubs_path
+
+    click_link('Edit')
+    expect(page).to have_content 'Editing Beer'
+    fill_in('beer_club_name', with:'Polololo')
+    click_button('Update Beer club')
+    expect(page).to have_content 'Polololo'
+  end
+
 end
