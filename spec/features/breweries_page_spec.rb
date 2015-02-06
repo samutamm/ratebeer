bc076f8c6cdfdf37e7cb8f14e1615ffc201bdc43
samutamm <samutamm@gmail.com>
2015-02-06 14:02:53 +0200

    More tests for breweries page-feature.

--- spec/features/breweries_page_spec.rb
+++ spec/features/breweries_page_spec.rb
@@ -15,6 +15,8 @@ describe "Breweries page" do
         FactoryGirl.create(:brewery, name: brewery_name, year: year += 1)
       end
 
+      FactoryGirl.create(:user)
+      sign_in(username:"Pekka", password:"Foobar1")
       visit breweries_path
     end
 
@@ -30,6 +32,24 @@ describe "Breweries page" do
 
       expect(page).to have_content "Koff"
       expect(page).to have_content "Established in 1897"
+    end
+
+    it 'can create new Brewery' do
+      click_link('New Brewery')
+      fill_in('brewery_name', with:'Panimo')
+      fill_in('brewery_year', with:1999)
+      expect{
+        click_button('Create Brewery')
+      }.to change{Brewery.count}.from(3).to(4)
+    end
+
+    it 'get redirection if given name missing.' do
+      click_link('New Brewery')
+      fill_in('brewery_name', with:'')
+      fill_in('brewery_year', with:1999)
+
+      click_button('Create Brewery')
+      expect(page).to have_content 'Name can\'t be blank'
     end
 
   end
--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -14,4 +14,17 @@ describe Brewery do
 
     expect(brewery).not_to be_valid
   end
+
+  it "creating brewery saves it to database." do
+    expect{
+      Brewery.create name:'Panimo', year:'1988'
+    }.to change{Brewery.count}.from(0).to(1)
+  end
+
+  it "destroying brewery deletes it from database." do
+    brewery = Brewery.create name:'Panimo', year:'1988'
+    expect{
+      Brewery.first.destroy
+    }.to change{Brewery.count}.from(1).to(0)
+  end
 end
