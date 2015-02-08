4f286f5d463b34edd542ac6948f32636bfe3508f
samutamm <samutamm@gmail.com>
2015-02-08 20:40:38 +0200

    Add check for empty search keywords.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,11 +4,16 @@ class PlacesController < ApplicationController
   end
 
   def search
-    @places = BeermappingApi.places_in(params[:city])
-    if @places.empty?
-      redirect_to places_path, notice: "No locations in #{params[:city]}"
+    city = params[:city]
+    if city.empty?
+      redirect_to places_path, notice: "Please give the name of city."
     else
-      render :index
+      @places = BeermappingApi.places_in(city)
+      if @places.empty?
+        redirect_to places_path, notice: "No locations in #{city}"
+      else
+        render :index
+      end
     end
   end
 end
