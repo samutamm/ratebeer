154010892b1bd08645eb1774b0e90a968d41e2d8
samutamm <samutamm@gmail.com>
2015-02-08 19:55:18 +0200

    Move rating_average.rb file from model/concerns to lib/

--- lib/rating_average.rb
+++ lib/rating_average.rb
@@ -4,6 +4,5 @@ module RatingAverage
   def average_rating
     return 0 if self.ratings.empty?
     self.ratings.map{|r| r.score}.sum / self.ratings.count.to_f
-    #self.ratings.inject(0.0) { |sum, n| sum += n.score  } / self.ratings.count
   end
 end
