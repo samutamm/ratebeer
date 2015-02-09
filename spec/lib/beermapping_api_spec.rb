d12741ed4ad4067890badc964161b7599caa39dd
samutamm <samutamm@gmail.com>
2015-02-09 18:06:35 +0200

    First test for BeerMappingApi with stubing with webmock.

---
+++ spec/lib/beermapping_api_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "BeermappingApi" do
+  it "When HTTP GET returns one entry, it is parsed and returned" do
+
+    canned_answer = <<-END_OF_STRING
+<?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>12411</id><name>Gallows Bird</name><status>Brewery</status><reviewlink>http://beermapping.com/maps/reviews/reviews.php?locid=12411</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=12411&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=12411&amp;d=1&amp;type=norm</blogmap><street>Merituulentie 30</street><city>Espoo</city><state></state><zip>02200</zip><country>Finland</country><phone>+358 9 412 3253</phone><overall>91.66665</overall><imagecount>0</imagecount></location></bmp_locations>
+    END_OF_STRING
+
+    stub_request(:get, /.*espoo/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
+
+    places = BeermappingApi.places_in("espoo")
+
+    expect(places.size).to eq(1)
+    place = places.first
+    expect(place.name).to eq("Gallows Bird")
+    expect(place.street).to eq("Merituulentie 30")
+  end
+
+end
