c93cd17c896b6ae9b8e17c5e95ecc0ad9aad7ebb
samutamm <samutamm@gmail.com>
2015-02-09 18:34:34 +0200

    Add Beermapping to use cache for avoid queries and raise performance.

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,5 +1,11 @@
 class BeermappingApi
+
   def self.places_in(city)
+    city = city.downcase
+    Rails.cache.fetch(city) { fetch_places_in(city) }
+  end
+
+  def self.fetch_places_in(city)
     url = 'http://stark-oasis-9187.herokuapp.com/api/'
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
