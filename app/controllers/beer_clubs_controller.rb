b1dbddc5dcdc0c2eb4551134f40e6c41a985de96
samutamm <samutamm@gmail.com>
2015-02-11 14:43:54 +0200

    Add 'end membership'-feature to beerClub show.html.erb

--- app/controllers/beer_clubs_controller.rb
+++ app/controllers/beer_clubs_controller.rb
@@ -11,10 +11,17 @@ class BeerClubsController < ApplicationController
   # GET /beer_clubs/1
   # GET /beer_clubs/1.json
   def show
-    if current_user && (not @beer_club.members.include?(current_user))
-      @membership = Membership.new
-      #@membership.user = current_user
-      @membership.beer_club = @beer_club
+    if current_user
+      if @beer_club.members.include?(current_user)
+        @beer_club.memberships.each do |m|
+          if m.user_id == current_user.id
+            @membership = m
+          end
+        end
+      else
+        @membership = Membership.new
+        @membership.beer_club = @beer_club
+      end
     end
   end
 
--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -57,9 +57,10 @@ class MembershipsController < ApplicationController
   # DELETE /memberships/1
   # DELETE /memberships/1.json
   def destroy
+    beer_club_id = @membership.beer_club_id
     @membership.destroy
     respond_to do |format|
-      format.html { redirect_to memberships_url, notice: 'Membership was successfully destroyed.' }
+      format.html { redirect_to beer_club_path(beer_club_id), notice: 'Membership was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
