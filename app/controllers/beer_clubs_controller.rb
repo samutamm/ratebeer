88f888674ec271fa413768ff8e77e65e974330a7
samutamm <samutamm@gmail.com>
2015-02-11 09:59:45 +0200

    Add 'join the club'-feature to beerClub-view.

--- app/controllers/beer_clubs_controller.rb
+++ app/controllers/beer_clubs_controller.rb
@@ -11,6 +11,11 @@ class BeerClubsController < ApplicationController
   # GET /beer_clubs/1
   # GET /beer_clubs/1.json
   def show
+    if current_user
+      @membership = Membership.new
+      @membership.user = current_user
+      @membership.beer_club = @beer_club
+    end
   end
 
   # GET /beer_clubs/new
