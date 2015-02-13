5b0198beb076adb446e3bb1ff05d538ae13614e0
samutamm <samutamm@gmail.com>
2015-02-13 12:11:57 +0200

    Fix database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213084658) do
+ActiveRecord::Schema.define(version: 20150213101123) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
