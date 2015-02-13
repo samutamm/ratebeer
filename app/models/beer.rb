d7a8fdf3017f5d6e518aaf0cb19cf9ef38010f71
samutamm <samutamm@gmail.com>
2015-02-13 12:25:17 +0200

    Style has many beers and beer belongs to style

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,6 +1,7 @@
 class Beer < ActiveRecord::Base
   include RatingAverage
-  
+
+  belongs_to :style
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
   has_many :raters, ->{ uniq }, through: :ratings, source: :user
