26d291f927c3f4d20828d13d24b1625b3b30b419
samutamm <samutamm@gmail.com>
2015-02-15 17:25:17 +0200

    Top beers-

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -8,6 +8,11 @@ class Beer < ActiveRecord::Base
 
   validates :name, :style_id, presence: true
 
+  def self.top(n)
+    sorted_by_rating_in_desc_order = Beer.all.sort_by{ |b| -(b.average_rating||0) }
+    sorted_by_rating_in_desc_order.take(n)
+  end
+
   def to_s
     "#{self.name} | #{brewery.name}"
   end
