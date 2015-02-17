eb82d5fa786de52a9822c9afa63c845537bf6200
samutamm <samutamm@gmail.com>
2015-02-17 16:16:16 +0200

    UserController-method toggle-banned to change banned status.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,21 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :ensure_that_signed_in, except: [:index, :show, :new, :create]
-  # GET /users
-  # GET /users.json
+
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -37,8 +30,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if user_params[:username].nil? and @user == current_user and @user.update(user_params)
@@ -51,8 +42,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.ratings.each {|x| x.destroy}
     @user.memberships.each {|x| x.destroy}
@@ -67,14 +56,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def toggle_banned
+    user = User.find(params[:id])
+    user.update_attribute :banned, (not user.banned)
+
+    new_status = user.banned ? "banned" : "not banned"
+
+    redirect_to :back, notice:"user banned status changed to #{new_status}"
+  end
+
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :password, :password_confirmation)
+      params.require(:user).permit(:username, :password, :password_confirmation, :banned)
     end
 end
